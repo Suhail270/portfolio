4a71d6131d25209913ae0385bf30a531df8ec277
Suhail <suhailz13ahmed@outlook.com>
2024-05-15 16:15:14 +0400

    updated index.html

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { About, Contact, Experience, Achievements, Hero, Navbar, Tech, Works, St
 
 const App = () => {
   return (
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
