26530a5bc5916af3603f6da73f295fe0ad75107a
Suhail <suhailz13ahmed@outlook.com>
2024-05-15 15:45:35 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom/BrowserRouter";
 
 import { About, Contact, Experience, Achievements, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
